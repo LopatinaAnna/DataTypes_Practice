51e58c695342ac68238b9372e84845c15442fa9c
Anna Lopatina <an.lopatina050@gmail.com>
2021-02-16 17:56:25 +0200

    Добавьте файлы проекта.

---
+++ Structures/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point[] points = new Point[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Point x: ");
+                double x = double.Parse(Console.ReadLine());
+                Console.Write("Point y: ");
+                double y = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                points[i] = new Point(x, y);
+            }
+
+            try
+            {
+                var square = new Square(points[0], points[1], points[2], points[3]);
+
+                Console.WriteLine("This is a square.");
+                Console.WriteLine($"Width: {square.GetWidth()}");
+                Console.WriteLine($"Height: {square.GetHeight()}");
+                Console.WriteLine($"Area: {square.GetArea()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Structures/Square.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Structures
+{
+    public class Square
+    {
+        public Point[] Points { get; }
+
+        public Square(Point a, Point b, Point c, Point d)
+        {
+            if (IsSquare(a, b, c, d))
+                Points = new[] { a, b, c, d };
+            else
+                throw new Exception("This is not a square.");
+        }
+
+        public double GetHeight()
+        {
+            return Math.Round(Math.Sqrt(Distance(Points[0], Points[1])), 2);
+        }
+
+        public double GetWidth()
+        {
+            return GetHeight();
+        }
+
+        public double GetArea()
+        {
+            return Math.Round(Math.Pow(GetHeight(), 2), 2);
+        }
+
+        private bool IsSquare(Point a, Point b, Point c, Point d)
+            => Distance(a, b) == Distance(b, c) &&
+            Distance(b, c) == Distance(c, d) &&
+            Distance(c, d) == Distance(d, a);
+
+        private double Distance(Point a, Point b) 
+            => Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2);
+    }
+}
