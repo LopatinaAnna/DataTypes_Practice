9bfca9f6e6bd4f327fb15c335d5e4de9990e5d9c
Anna Lopatina <an.lopatina050@gmail.com>
2021-02-18 19:22:14 +0200

    String and StringBuilder compare

---
+++ Strings_1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace Strings_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter string: ");
+            //var inputString = Console.ReadLine();
+            //var stringBuilder = new StringBuilder(inputString);
+
+            //Console.Write("\nEnter char for removing: ");
+            //var charToRemove = Console.ReadLine();
+
+            var inputString = "Although StringBuilder and String both represent sequences of characters, they are implemented " +
+                "differently. String is an immutable type. That is, each operation that appears to modify a String object actually " +
+                "creates a new string. For example, the call to the String.Concat method in the following C# example appears " +
+                "to change the value of a string variable named value. In fact, the Concat method returns a value object that has " +
+                "a different value and address from the value object that was passed to the method. For routines that perform " +
+                "extensive string manipulation (such as apps that modify a string numerous times in a loop), modifying a string " +
+                "repeatedly can exact a significant performance penalty. The alternative is to use StringBuilder, which is " +
+                "a mutable string class. Mutability means that once an instance of the class has been created, it can be modified " +
+                "by appending, removing, replacing, or inserting characters. A StringBuilder object maintains a buffer to " +
+                "accommodate expansions to the string. New data is appended to the buffer if room is available; otherwise, " +
+                "a new, larger buffer is allocated, data from the original buffer is copied to the new buffer, and the new " +
+                "data is then appended to the new buffer.";
+
+            var stringBuilder = new StringBuilder(inputString);
+            var charToRemove = "a";
+
+            var timer = new Stopwatch();
+
+            timer.Start();
+            inputString.Replace(charToRemove, string.Empty);
+            timer.Stop();
+
+            Console.WriteLine($"String: {timer.Elapsed.TotalMilliseconds} ms");
+
+            timer.Restart();
+            stringBuilder.Replace(charToRemove, string.Empty);
+            timer.Stop();
+
+            Console.WriteLine($"StringBuilder: {timer.Elapsed.TotalMilliseconds} ms");
+        }
+    }
+}
