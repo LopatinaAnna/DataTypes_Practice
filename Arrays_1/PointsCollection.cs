94b54a76a6b044931c6e447c71d0202b30e739f2
Anna Lopatina <an.lopatina050@gmail.com>
2021-02-22 17:18:36 +0200

    Add and insert implementation

--- Arrays_1/PointsCollection.cs
+++ Arrays_1/PointsCollection.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Arrays_1
@@ -6,20 +7,53 @@ namespace Arrays_1
     public class PointsCollection : IEnumerable<Point>
     {
         private Point[] points;
+        private int length;
 
-        public PointsCollection(int length)
+        public int Length => length;
+
+        public int Capacity => points.Length;
+
+        public PointsCollection()
         {
-            points = new Point[length];
+            points = new Point[16];
+            length = 0;
+        }
+
+        public PointsCollection(int capacity)
+        {
+            points = new Point[capacity];
+            length = 0;
         }
 
         public void Add(Point point)
         {
+            if (length == points.Length)
+            {
+                int capacity = points.Length < 16 ? 16 : points.Length * 2;
 
+                Array.Resize(ref points, capacity);
+            }
+
+            points[length++] = point;
         }
 
         public void Insert(Point point, int index)
         {
+            if (length == points.Length)
+            {
+                int capacity = points.Length < 16 ? 16 : points.Length * 2;
 
+                Array.Resize(ref points, capacity);
+            }
+
+            for (int i = length; i > index; i--)
+            {
+                points[i] = points[i - 1];
+            }
+
+            points[index] = point;
+
+            length++;
         }
 
         public void Remove(Point point)
@@ -34,7 +68,7 @@ namespace Arrays_1
 
         public IEnumerator<Point> GetEnumerator()
         {
-            for (int i = 0; i < points.Length; i++)
+            for (int i = 0; i < length; i++)
             {
                 yield return points[i];
             }
--- Arrays_1/Program.cs
+++ Arrays_1/Program.cs
@@ -6,7 +6,36 @@ namespace Arrays_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var points = new PointsCollection(5)
+            {
+                new Point(0, 1),
+                new Point(1, 2), 
+                new Point(2, 3),
+                new Point(3, 4),
+                new Point(4, 5)
+            };
+            Print(points, "Create new collection");
+
+            points.Add(new Point(5, 6));
+            Print(points, "Add new value and resize collection");
+
+            points.Insert(new Point(6, 7), 0);
+            Print(points, "Insert new value (6, 7) at the beginning");
+
+            points.Insert(new Point(7, 8), 2);
+            Print(points, "Insert new value (7, 8) in the middle");
+
+            points.Insert(new Point(8, 9), 8);
+            Print(points, "Insert new value (8, 9) at the end"); ;
+        }
+
+        static void Print(PointsCollection points, string message)
+        {
+            Console.WriteLine($"{message}, length: {points.Length}, capacity: {points.Capacity}");
+            foreach (var item in points)
+            {
+                Console.WriteLine($"({item.x},{item.y})");
+            }
         }
     }
 }
