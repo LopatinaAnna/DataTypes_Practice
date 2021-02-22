527334a106e5e986c79326fc337ef98dc82f5857
Anna Lopatina <an.lopatina050@gmail.com>
2021-02-22 13:51:13 +0200

    Initial array project

---
+++ Arrays_1/Point.cs
@@ -0,0 +1,15 @@
+﻿namespace Arrays_1
+{
+    public struct Point
+    {
+        public double x;
+
+        public double y;
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ Arrays_1/PointsCollection.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Arrays_1
+{
+    public class PointsCollection : IEnumerable<Point>
+    {
+        private Point[] points;
+
+        public PointsCollection(int length)
+        {
+            points = new Point[length];
+        }
+
+        public void Add(Point point)
+        {
+
+        }
+
+        public void Insert(Point point, int index)
+        {
+
+        }
+
+        public void Remove(Point point)
+        {
+
+        }
+
+        public void RemoveAt(int index)
+        {
+
+        }
+
+        public IEnumerator<Point> GetEnumerator()
+        {
+            for (int i = 0; i < points.Length; i++)
+            {
+                yield return points[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return points.GetEnumerator();
+        }
+    }
+}
